0a80e903668e82a0da6d7fa64e4f7db0e9a9d551
PraDoL2J <214189133+JulioPradoL2j@users.noreply.github.com>
2025-07-31 12:19:36 -0300

    Initial commit

---
+++ java/mods/main.java
@@ -0,0 +1,26 @@
+package mods;
+
+import ext.mods.extensions.interfaces.L2JExtension;
+
+public class main implements L2JExtension
+{
+	
+	@Override
+	public String getName()
+	{
+		return "main";
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+	
+	@Override
+	public void onLoad()
+	{
+		
+	}
+	
+}
